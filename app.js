ffffc4331112049aa63fb14bead6887dbc8f4733
Sham Reddy <techsabha.com@gmail.com>
2015-09-14 20:10:19 +0530

    Next version

--- app.js
+++ app.js
@@ -6,16 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,3 +50,10 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
+var youtubeStream = require('youtube-audio-stream')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.send("Hello World!!");
+
+  var getAudio = function(req, res) {
+ 
+  var requestUrl = 'http://youtube.com/watch?v=-IPKPz1pujM' ;//+ req.params.videoId;
+ 
+  try {
+  	console.log("Before Download");
+    youtubeStream(requestUrl).pipe(res);
+    console.log("After Download");
+  } catch(exception) {
+    res.status(500).send(exception);
+  }
+ 
+};
+getAudio();
 });
 
 module.exports = router;
